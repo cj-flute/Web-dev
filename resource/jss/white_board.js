ed1f82ac8460c8706257c7681d7f8fbe2f355520
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-29 18:18:34 +0100

    send up

---
+++ resource/jss/white_board.js
@@ -0,0 +1,24 @@
+let playGround = document.querySelector(".display-ground");
+// create elements
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+const text = document.createTextNode("Welcome");
+        
+const account1 = {
+    owner: "Chijioke",
+    movement: [200, 450, -400, 3000, -650, -120, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111,
+};
+
+
+const account2 = {
+    owner: "user",
+    movementL: [5000, 3400, -150, -700, -3210, -1000],
+    interestRate: 1.5,
+    pin: 2222
+};
+
+
+
+
